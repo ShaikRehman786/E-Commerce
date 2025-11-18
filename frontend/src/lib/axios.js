8ec4149ea6cf97d005f8f38a78d57af2a2731ee0
Shaik Rehman <shaikrehman78609@gmail.com>
2025-11-18 21:06:25 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://e-commerce-nque.onrender.com",
+      : "https://e-commerce-nque.onrender.com/api",
   withCredentials: true,
 });
 
